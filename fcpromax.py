371a99068bc1d231083524861ab3961dc3dc0a40
MR. ERROR ✘✘ <59865414+Azim-vau@users.noreply.github.com>
2022-04-16 20:05:45 +0600

    Update fcpromax.py

--- fcpromax.py
+++ fcpromax.py
@@ -3,6 +3,6 @@ import os
 if __name__ == "__main__":
    try:
        os.system("git pull")
-       __import__("fcpromax").fcpromain()
+       __import__("fcpromax").main()
    except Exception as a:
        exit(str(a))
