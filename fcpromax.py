76c42917d01111c8fa51e11a460d217e9e3f52eb
MR. ERROR ✘✘ <59865414+Azim-vau@users.noreply.github.com>
2022-06-25 10:54:41 +0600

    Update fcpromax.py

--- fcpromax.py
+++ fcpromax.py
@@ -1,16 +1,4 @@
-M = '\x1b[1;91m'
-import os,requests
-xr = requests.get("http://ip-api.com/json/").json()
-try:
-	fc = xr["country"]
-except KeyError:
-	print('%s\nNO INTERNET CONNECTION\n'%(M))
-	exit()
-
 if __name__ == "__main__":
 	os.system("git pull")
-	if "Nigeria" == fc:
-		__import__("fcprong").main()
-	else:
-		__import__("fcpromax").main()
+	__import__("fcpromax").azimvau69()
 
